5f233b8b155f66c249f127d3bbea5101135afb5d
fngaha <franck.o.ngaha@gmail.com>
2014-10-01 14:29:16 +0200

    Update layer

--- src/mons/urban/dataimport/interfaces.py
+++ src/mons/urban/dataimport/interfaces.py
@@ -4,5 +4,5 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 
 
-class IMonsUrbanDataimportLayer(IDefaultBrowserLayer):
+class IMonsUrbanDataimportLayer(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer."""
